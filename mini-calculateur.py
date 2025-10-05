86616cc9a0f8e197da8867198d3c75ed80191108
gabriel <tchidjouivangabriel@gmail.com>
2025-10-05 14:32:57 +0100

    ici c'est un autre calculateur qui effectue toutes les operations possibles sans possibilité de choix

---
+++ mini-calculateur.py
@@ -0,0 +1,64 @@
+# ceci est ma premiere experience avec pyton le 05_10_2025 à 12h
+
+
+
+
+# declaration des variables
+a=5
+b=10
+nom="Ivan"
+
+# affichage des variables
+print(a)
+print(b)
+print(nom)
+
+# operations mathematiques
+somme= a+b
+difference= a-b
+produit= a*b
+division= a/b
+
+# affichage des resultats
+print("somme:", somme)
+print("difference:", difference)
+print("produit:", produit)
+print("division:", division)
+
+# division entiere sans virgule
+div_entiere = a//b
+
+# affichage de la division entiere
+print("div_entiere:", div_entiere)
+ # ddemander a l'utilisateur de saisir deux nombres
+a = float(input("entrer le premier nombre:"))
+b = float(input("entrer le deuxieme nombre:"))
+
+# affichage des resultats
+somme= a+b
+difference= a-b
+produit= a*b
+division= a/b
+
+print("somme:", somme)
+print("difference:", difference)
+print("produit:", produit)
+print("division:", division)
+
+# autres operations
+c=33
+d=22
+puissance =a**b
+reste_de_la_div =a%b
+
+# affichage des resultats
+print("puissance=", puissance)
+print("reste=", reste_de_la_div)
+
+
+
+
+
+
+ 
+ 
